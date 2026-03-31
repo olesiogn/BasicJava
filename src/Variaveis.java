a4724123cd311743084e4389f15ec9f5036f772f
Olesio <olesio.gardenghi.neto@nttdata.com>
2026-03-31 14:23:19 -0300

    feat: variaveis

---
+++ src/Variaveis.java
@@ -0,0 +1,33 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+        // Imprime "Hello, World!" no console
+        System.out.println("Hello, World!");
+
+        // Variáveis de diferentes tipos
+        // Variável do tipo inteiro
+        int idade = 28;
+        System.out.println("Sua idade é: " + idade);
+
+        // Variável do tipo String
+        String nome = "Fulano";
+        System.out.println("Meu nome é: " + nome);
+
+        // Variável do tipo float
+        float altura = 1.81f;
+        System.out.println("Minha altura é: " + altura + " metros");
+
+        // Variável do tipo double
+        double peso = 50.5;
+        System.out.println("Meu peso é: " + peso + " kg");
+
+        // Variável do tipo boolean
+        boolean estudante = false;
+        System.out.println("Sou estudante? " + estudante);
+
+        // Variável do tipo char
+        char genero = 'M';
+        System.out.println("Meu gênero é: " + genero);
+
+    }
+}
