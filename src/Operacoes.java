2e15e269daa1dd7736e5789a483a6a5d8c96493a
Olesio <olesio.gardenghi.neto@nttdata.com>
2026-03-31 14:32:40 -0300

    refactor: operações

--- src/Operacoes.java
+++ src/Operacoes.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class Operacoes {
+    public static void main(String[] args) {
+        // Operações matemáticas
+        int a = 10;
+        int b = 5;
+
+        // Adição
+        int soma = a + b;
+        System.out.println("Soma: " + soma);
+
+        // Subtração
+        int subtracao = a - b;
+        System.out.println("Subtração: " + subtracao);
+
+        // Multiplicação
+        int multiplicacao = a * b;
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        // Divisão
+        int divisao = a / b;
+        System.out.println("Divisão: " + divisao);
+
+        // Resto da divisão
+        int resto = a % b;
+        System.out.println("Resto da divisão: " + resto);
+
+        String nome1 = "Fulano";
+        String nome2 = " da Silva";
+
+        // Concatenação de strings
+        String nomeCompleto = nome1 + nome2;
+        System.out.println("Nome completo: " + nomeCompleto);
+    }
 }
