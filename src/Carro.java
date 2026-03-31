7f28a3860beac2e0d4f5afbc37f2c9f52def940c
Olesio <olesio.gardenghi.neto@nttdata.com>
2026-03-31 16:28:13 -0300

    feat: classe e herança

---
+++ src/Carro.java
@@ -0,0 +1,39 @@
+public class Carro {
+    private String cor;
+    private String modelo;
+    private int ano;
+
+    public Carro() {
+    }
+
+    public Carro(String cor, String modelo, int ano) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto Carro
+        Carro meuCarro = new Carro();
+        meuCarro.setCor("Vermelho");
+        meuCarro.setModelo("Sedan");
+        meuCarro.setAno(2022);
+
+        System.out.println("Informações do carro:");
+        System.out.println("Cor: " + meuCarro.getCor());
+        System.out.println("Modelo: " + meuCarro.getModelo());
+        System.out.println("Ano: " + meuCarro.getAno());
+
+        // Criando outro carro usando o construtor com parâmetros
+        Carro outroCarro = new Carro("Preto", "SUV", 2024);
+        System.out.println("\nOutro carro:");
+        System.out.println("Cor: " + outroCarro.getCor());
+        System.out.println("Modelo: " + outroCarro.getModelo());
+        System.out.println("Ano: " + outroCarro.getAno());
+
+        // Exemplo de herança: CarroEsportivo
+        CarroEsportivo esportivo = new CarroEsportivo("Amarelo", "Coupé", 2023, 350);
+        System.out.println("\nCarro esportivo:");
+        System.out.println("Cor: " + esportivo.getCor());
+        System.out.println("Modelo: " + esportivo.getModelo());
+        System.out.println("Ano: " + esportivo.getAno());
+        System.out.println("Potência: " + esportivo.getPotencia() + " cv");
+    }
+}
